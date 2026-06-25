71c7ada303e59936c382197f7e254ccdde5e892e
João Iop Beltrame <115894856+JoaoIopBeltrame@users.noreply.github.com>
2026-06-24 22:23:20 -0400

    Add matrix addition and multiplication functionality
    Refactor matrix operations to include addition and multiplication functions. Update main to display results of both operations.

--- Calculadora-Matriz.py
+++ Calculadora-Matriz.py
@@ -20,11 +20,22 @@ def definirMatrizB():
             print("Digite um numero inteiro")
         return mB
 
-def matrizMenosMais(mA, mB, operador): #colocar dentro do main e o inpt de quantoQUANTO vai colocar o valor dentro disso
+def matrizMenosMais(mA, mB, operador):
     return [
         [operador(mA[l][c], mB[l][c]) for c in range(len(mA[0]))
         ] for l in range(len(mA))  
     ]
+
+
+
+def matrizMult(mA, mB, operador): #num de coluna tem que ser igual a de linha linha de A multiplica cada coluna de B e soma  os coisas de coluna diferentes na mesma linha
+        if (mA[len(l)] == mB[len(c)]) and ((mA[c] == mB[l]) or (mA[c] != mB[l])):
+            return[
+                [operador(mA[l][c], mB[l][c]) for c in range(len(mA[0]))
+                ] for l in range(len(mA))
+
+            ]
+
 
 def main():
     mA = definirMatrizA()
@@ -36,8 +47,51 @@ def main():
     res = matrizMenosMais(mA, mB, lambda mA, mB: mA + mB)
     print(f"=====C=====")
     print(*res, sep="\n")
+    print(f"=====D=====")
+    res2 = matrizMult(mA, mB, lambda mA, mB: mA * mB)
+    print(*res2, sep="\n")
 
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+# matriz = []
+# for l in range(10):
+#     linha = []
+#     for c in range(10):
+#         linha.append(0)
+#     matriz.append(linha)
+
+# int x,y,m1[x][y],m2[x][y];
+# for (x=0;x<10;x++){
+#     for (y=0;y<10;y++){
+#         printf("digite o valor da matriz;\n" &m1[x][y]);
+#         scanf("%d", m1[x][y]);
+#     }
+# for (x=0;x<10;x++){
+#     for (y=0;y<10;y++){
+#         printf("digite o valor da matriz;\n" &m2[x][y]);
+#         scanf("%d", m2[x][y]);
+#     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
